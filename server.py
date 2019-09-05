b6ce7c7bc8df17c1a3cef37f53ab963720e99140
edacicekli <38438859+edacicekli@users.noreply.github.com>
2019-09-05 17:48:25 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import socket
 import struct
-import os
+import os # OS tarafından sağlanan bir çok hizmeti Python içerisinde kolay bir şekilde kullanmamızı sağlayacak nesneleri barındırır.
 
-if os.path.exists("/tmp/python_unix_sockets_example"):
-       os.remove("/tmp/python_unix_sockets_example")
-s = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+if os.path.exists("/tmp/python_unix_sockets_example"): # path.exists ile bir dosya ve klasörün var olup olmadığını kontrol edersiniz.
+       os.remove("/tmp/python_unix_sockets_example") 
+s = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM) 
 
 
 s.bind("/tmp/python_unix_sockets_example")
